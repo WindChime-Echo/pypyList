89b58a67b24227255d2a51d4c7973a07cedb9bc6
Echo <chroom@foxmail.com>
2023-10-22 17:41:43 +0800

    解决请求路径

--- src/service/pypydance/index.js
+++ src/service/pypydance/index.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export default function getPypyDance() {
   return new Promise((reslove, reject) => {
-    axios.get('https://pypy.kksk.ink/api/v1/songs')
+    // https://jd.pypy.moe/api/v1/songs
+    // 需要自行对以上链接进行代理，否则有跨域问题
+    axios.get(window.location.origin + '/api/v1/songs')
       .then(function (response) {
         reslove(response.data)
       })
