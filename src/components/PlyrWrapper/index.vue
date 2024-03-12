b81f199991d47e223147b32de0f991b614e5e2cf
Echo <chroom@foxmail.com>
2024-03-12 11:46:45 +0800

    解决下一首丢失全屏问题

--- src/components/PlyrWrapper/index.vue
+++ src/components/PlyrWrapper/index.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <div ref="videoContainer" v-html="videoHtml"></div>
   </div>
@@ -37,6 +37,9 @@ const initPlyr = async () => {
   if (videoContainer.value) {
     plyrInstance = new Plyr(videoContainer.value.querySelector("video"), {
       autoplay: true,
+      fullscreen: {
+        enabled: true,
+      },
     })
 
     // 根据 flip 属性应用翻转效果
@@ -70,5 +73,99 @@ onUnmounted(() => {
     plyrInstance.destroy()
   }
 })
+</script> -->
+<template>
+  <div>
+    <video ref="plyrVideo" controls class="plyr__video-embed">
+      <source :src="props.videoUrl" type="video/mp4" />
+    </video>
+  </div>
+</template>
+
+<script setup>
+import { ref, watch, onMounted, onUnmounted, nextTick } from "vue"
+import Plyr from "plyr"
+import "plyr/dist/plyr.css"
+
+const props = defineProps({
+  videoUrl: String,
+  flip: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(["videoEnded"])
+const plyrVideo = ref(null)
+
+let plyrInstance = null
+
+// 定义静音然后延迟取消静音的函数
+const muteAndUnmuteAfterDelay = (delay = 1000) => {
+  if (plyrInstance) {
+    plyrInstance.muted = true // 开始时静音
+    setTimeout(() => {
+      plyrInstance.muted = false // 延迟后取消静音
+      plyrInstance.volume = 1 // 可选：同时设置音量为最大
+    }, delay)
+  }
+}
+
+const initPlyr = () => {
+  nextTick(() => {
+    if (plyrInstance) {
+      plyrInstance.destroy()
+    }
+    if (plyrVideo.value) {
+      plyrInstance = new Plyr(plyrVideo.value, {
+        autoplay: true,
+      })
+
+      plyrInstance.on("ended", () => {
+        emit("videoEnded")
+      })
+
+      muteAndUnmuteAfterDelay() // 初始化 Plyr 后调用静音/取消静音逻辑
+
+      applyFlipEffect()
+    }
+  })
+}
+
+const applyFlipEffect = () => {
+  if (props.flip) {
+    plyrVideo.value.style.transform = "scaleX(-1)"
+  } else {
+    plyrVideo.value.style.transform = "scaleX(1)"
+  }
+}
+
+watch(
+  () => props.videoUrl,
+  (newVal) => {
+    if (plyrInstance && newVal) {
+      plyrInstance.source = {
+        type: "video",
+        sources: [
+          {
+            src: newVal,
+            type: "video/mp4",
+          },
+        ],
+      }
+      muteAndUnmuteAfterDelay() // 当视频 URL 更新时再次应用静音/取消静音逻辑
+    }
+  },
+  { immediate: true }
+)
+
+onMounted(() => {
+  initPlyr()
+})
+
+onUnmounted(() => {
+  if (plyrInstance) {
+    plyrInstance.destroy()
+  }
+})
 </script>
-<style scoped></style>
